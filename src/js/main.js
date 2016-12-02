d1496d85abb11d66187ae1df20db862a16d26004
Paul Molluzzo <paul@molluzzo.com>
2016-12-02 11:34:43 -0500

    Move JS to separate file and inline with gulp

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var head = document.querySelector('head');
+var icons = [
+  {rel: 'icon', href: './assets/favicon.ico', type: 'image/x-icon'},
+  {rel: 'apple-touch-icon', href: './assets/apple-icon-57x57.png', type: 'image/png', sizes: '57x57'},
+  {rel: 'apple-touch-icon', href: './assets/apple-icon-180x180.png', type: 'image/png', sizes: '180x180'},
+  {rel: 'icon', href: './assets/android-icon-192x192.png', type: 'image/png'},
+];
+
+icons.forEach(function(icon) {
+  var link = document.createElement('link');
+  var keys = Object.keys(icon);
+
+  keys.forEach(function(key) {
+    link.setAttribute(key, icon[key]);
+  });
+
+  head.append(link);
+});
