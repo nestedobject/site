6f7268437a21160dd1fe40b7300a5e959e876479
Paul Molluzzo <paul@molluzzo.com>
2016-12-05 11:46:18 -0500

    Fix xo errors in JS

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,14 @@ var icons = [
   {rel: 'icon', href: './assets/favicon.ico', type: 'image/x-icon'},
   {rel: 'apple-touch-icon', href: './assets/apple-icon-57x57.png', type: 'image/png', sizes: '57x57'},
   {rel: 'apple-touch-icon', href: './assets/apple-icon-180x180.png', type: 'image/png', sizes: '180x180'},
-  {rel: 'icon', href: './assets/android-icon-192x192.png', type: 'image/png'},
+  {rel: 'icon', href: './assets/android-icon-192x192.png', type: 'image/png'}
 ];
 
-icons.forEach(function(icon) {
+icons.forEach(function (icon) {
   var link = document.createElement('link');
   var keys = Object.keys(icon);
 
-  keys.forEach(function(key) {
+  keys.forEach(function (key) {
     link.setAttribute(key, icon[key]);
   });
 
