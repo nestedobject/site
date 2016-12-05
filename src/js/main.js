7324d4b407eda43f8d6fde75487bed6a2e6bda75
Paul Molluzzo <paul@molluzzo.com>
2016-12-05 14:50:19 -0500

    Use link to load favicon

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@
 
 var head = document.querySelector('head');
 var icons = [
-  {rel: 'icon', href: './assets/favicon.ico', type: 'image/x-icon'},
   {rel: 'apple-touch-icon', href: './assets/apple-icon-57x57.png', type: 'image/png', sizes: '57x57'},
   {rel: 'apple-touch-icon', href: './assets/apple-icon-180x180.png', type: 'image/png', sizes: '180x180'},
   {rel: 'icon', href: './assets/android-icon-192x192.png', type: 'image/png'}
