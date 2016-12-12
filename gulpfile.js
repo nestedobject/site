47ff62773371c09179f6bc5fa65a17fc6184f53a
Paul Molluzzo <paul@molluzzo.com>
2016-12-12 16:48:17 -0500

    Re-add SCSS gulp and deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@
 
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
 const inlinesource = require('gulp-inline-source');
 
-gulp.task('minify', () => {
+gulp.task('sass', function () {
+  return gulp.src('scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('minify', ['sass'], () => {
   return gulp.src('src/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
     .pipe(inlinesource())
@@ -16,14 +23,18 @@ gulp.task('assets', () => {
     .pipe(gulp.dest('dist/assets'));
 });
 
+gulp.task('sass:watch', () => {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
 gulp.task('minify:watch', () => {
   gulp.watch('src/**/*.html', ['minify']);
 });
 
-gulp.task('default', ['minify', 'assets'], () => {
+gulp.task('default', ['sass', 'minify', 'assets'], () => {
   return;
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['src/**/*.css', 'src/**/*.html', 'src/assets/*.*'], ['minify', 'assets']);
+  gulp.watch(['scss/**/*.scss', 'src/**/*.html', 'src/assets/*.*'], ['sass', 'minify', 'assets']);
 });
