7fe277e86ce6783ac603c3a897eaa39646cb7866
Paul Molluzzo <paulmolluzzo@users.noreply.github.com>
2016-12-14 09:41:19 -0500

    Add auto-prefix (#2)
    * Re-add SCSS gulp and deps
    
    * Remove generated CSS file
    
    * Update color vars and use in animations
    
    * Add auto-prefixing
    
    * Fix xo errors; add gulp dep
    
    * Don't remove 'unused' prefixes
    
    * Add small negative delay to fix iOS bug
    
    * Undo delay hack
    
    * Fix polygon delays in clients
    
    * Remove delays and time out animations to work on iOS

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@
 
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
 const inlinesource = require('gulp-inline-source');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('minify', () => {
+gulp.task('sass', function () {
+  return gulp.src('scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      remove: false,
+      browsers: ['last 2 versions', '> 5%', 'iOS 7'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('minify', ['sass'], () => {
   return gulp.src('src/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
     .pipe(inlinesource())
@@ -16,14 +29,18 @@ gulp.task('assets', () => {
     .pipe(gulp.dest('dist/assets'));
 });
 
+gulp.task('sass:watch', () => {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
 gulp.task('minify:watch', () => {
   gulp.watch('src/**/*.html', ['minify']);
 });
 
-gulp.task('default', ['minify', 'assets'], () => {
+gulp.task('default', ['sass', 'minify', 'assets'], () => {
   return;
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['src/**/*.css', 'src/**/*.html', 'src/assets/*.*'], ['minify', 'assets']);
+  gulp.watch(['scss/**/*.scss', 'src/**/*.html', 'src/assets/*.*'], ['sass', 'minify', 'assets']);
 });
